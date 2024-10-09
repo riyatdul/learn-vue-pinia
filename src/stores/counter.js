b197aaae76dee0a68c68e4376dce692f0a2589cb
riyatdul <alriyatdul@gmail.com>
2024-10-09 14:02:42 +0700

    feature/counter

--- src/stores/counter.js
+++ src/stores/counter.js
@@ -2,11 +2,18 @@ import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
 export const useCounterStore = defineStore('counter', () => {
+  //state
   const count = ref(0)
+  //getters
   const doubleCount = computed(() => count.value * 2)
+  //actions/methods
   function increment() {
     count.value++
   }
 
-  return { count, doubleCount, increment }
+  function $reset() {
+    count.value = 0
+  }
+
+  return { count, doubleCount, increment, $reset }
 })
---
+++ src/views/CounterView.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { useCounterStore } from '@/stores/counter';
+
+const counterStore = useCounterStore()
+
+</script>
+
+<template>
+    <h1>Count : {{ counterStore.count }}</h1>
+    <h1>Double Count : {{ counterStore.doubleCount }}</h1>
+    <button @click="counterStore.increment()">Increment</button>
+    <button @click="counterStore.$reset()">RESET</button>
+</template>
